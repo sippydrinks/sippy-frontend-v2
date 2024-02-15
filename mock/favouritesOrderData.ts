515fcadac2b025b56dcfa2f8f755c48ace4c6a63
Dominic <dominicezeifejafor@gmail.com>
2024-02-15 03:48:20 +0100

    Added svgs, fonts styles and mock data

---
+++ mock/favouritesOrderData.ts
@@ -0,0 +1,46 @@
+
+// eslint-disable-next-line
+export default [
+    {
+		id: 1,
+		productName: "Coke zero",
+		productSize: "30CL",
+		productPrice: 350,
+		productImage: "/svgs/cart-product-image.svg",
+	},
+	{
+		id: 2,
+		productName: "Yoghurt",
+		productSize: "50CL",
+		productPrice: 850,
+		productImage: "/svgs/cart-product-image.svg",
+	},
+	{
+		id: 3,
+		productName: "Chivita",
+		productSize: "35CL",
+		productPrice: 800,
+		productImage: "/svgs/cart-product-image.svg",
+	},
+	{
+		id: 4,
+		productName: "Amaarula",
+		productSize: "150CL",
+		productPrice: 4200,
+		productImage: "/svgs/cart-product-image.svg",
+	},
+	{
+		id: 5,
+		productName: "Tequila",
+		productSize: "50CL",
+		productPrice: 2700,
+		productImage: "/svgs/cart-product-image.svg",
+	},
+	{
+		id: 6,
+		productName: "Chivita",
+		productSize: "35CL",
+		productPrice: 450,
+		productImage: "/svgs/cart-product-image.svg",
+	},
+]
---
+++ mock/index.ts
@@ -0,0 +1,34 @@
+import AlcoholicDrinkData from "./AlcoholicDrinkData"
+import ProductData from "./ProductData"
+import productListData from "./productListData"
+import reviewData from "./reviewData"
+import SoftDrinksData from "./SoftDrinksData"
+import navItems from "./navItems"
+import cartData from "./cartData"
+import accountData from "./accountData"
+import ongoingOrdersData from "./ongoingOrdersData"
+import cancelledOrderData from "./cancelledOrdersData"
+import completedOrdersData from "./completedOrdersData"
+import favouritesOrderData from "./favouritesOrderData"
+import couponCodes from "./couponCodes"
+import { sizeFilterData, priceFilterData, sortFilterData, brandFilterData } from "./filterData"
+
+export { 
+    AlcoholicDrinkData,
+    cartData,
+    accountData,
+    ProductData,
+    productListData,
+    reviewData,
+    SoftDrinksData,
+    navItems,
+    ongoingOrdersData,
+    cancelledOrderData,
+    completedOrdersData,
+    favouritesOrderData,
+    couponCodes,
+    sizeFilterData,
+    priceFilterData,
+    sortFilterData,
+    brandFilterData,
+}
