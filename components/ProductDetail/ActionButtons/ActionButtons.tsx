42b0069bab65b25006f92ae41f468c9c4d0384e9
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2024-03-16 09:50:34 +0100

    Add dynamic sticky scroll for action button container

--- components/ProductDetail/ActionButtons/ActionButtons.tsx
+++ components/ProductDetail/ActionButtons/ActionButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Counter, Button } from '@/shared';
 import { useGlobalContext } from '@/contexts/AppContext';
 import styles from './ActionButtons.module.scss';
@@ -15,8 +15,49 @@ const ActionButtons = ({ price }: { price: number }) => {
 		setCounter((counter) => (counter - 1 > 0 ? counter - 1 : counter));
 	};
 
+	const bottomRef = useRef<HTMLDivElement>(null);
+	const targetRef = document.getElementById('target_container')!;
+	const [isFixed, setIsFixed] = useState(false);
+	const [bottomPosition, setBottomPosition] = useState(0);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				// Update our state when target element is visible and fully intersecting
+				setIsFixed(entry.isIntersecting);
+			},
+			{ threshold: [1] } // Trigger when all part of the destination element is visible
+		);
+
+		if (targetRef) {
+			observer.observe(targetRef);
+		}
+
+		return () => {
+			if (targetRef) {
+				observer.unobserve(targetRef);
+			}
+		};
+	}, [targetRef]);
+
+	useEffect(() => {
+		const calculateBottomPosition = () => {
+			if (targetRef && bottomRef.current) {
+				const targetRect = targetRef.getBoundingClientRect();
+				const viewportHeight = window.innerHeight;
+
+				// Set bottom position of the action buttons container to the distance between the bottom of the target container and the bottom of the viewport
+				setBottomPosition(viewportHeight - targetRect.bottom);
+			}
+		};
+
+		calculateBottomPosition();
+		window.addEventListener('scroll', calculateBottomPosition);
+		return () => window.removeEventListener('scroll', calculateBottomPosition);
+	}, [isFixed]);
+
 	return (
-		<div className={styles.container}>
+		<div ref={bottomRef} className={styles.container} style={{ bottom: isFixed ? bottomPosition + 'px' : 0 }}>
 			<div className={styles.counterAndBtns}>
 				<div>
 					<div>
@@ -41,28 +82,9 @@ const ActionButtons = ({ price }: { price: number }) => {
 					<Bonanza />
 				</section>
 			</div>
+			<div className='bottom-placeholder' />
 		</div>
 	);
 };
 
 export default ActionButtons;
-{
-	/* <div className={styles.counterAndBtns_mob}>
-			<section className={styles.bonanza_container}>
-				<Bonanza />
-			</section>
-			<div className={styles.counter_mob}>
-				<h3>₦10,000</h3>
-				<Counter counterValue={counter} onDecrement={handleCountDecrease} onIncrement={handleCountIncrease} />
-			</div>
-			<div className={styles.btns_container_mob}>
-				<Button buttonType='primary' className={styles.addToCartBtn}>
-					<h3 className={styles.addToCartText}>Add to cart</h3>
-				</Button>
-
-				<Button buttonType='primary' className={styles.buyNowBtn}>
-					<h4>Buy now</h4>
-				</Button>
-			</div>
-		</div> */
-}
--- components/ProductDetail/ProductDetail.tsx
+++ components/ProductDetail/ProductDetail.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useGlobalContext } from '@/contexts/AppContext';
 import { Carousel, RelatedProducts } from '@/shared';
 import { ProductDetailComponent } from '.';
-
+import styles from './ProductDetail.module.scss';
 const ProductDetail = () => {
 	const { drinkType } = useGlobalContext();
 	return (
@@ -10,6 +10,7 @@ const ProductDetail = () => {
 			<ProductDetailComponent />
 			<Carousel title='similar products' isBorder={true} type='small' icon1='/svgs/StarOrange.svg' icon2='/svgs/StarOrange.svg' />
 			<RelatedProducts type='soft' />
+			<section id='target_container' className={styles.target_container}></section>
 			{/* 	 <ActionButtons /> */}
 		</>
 	);
