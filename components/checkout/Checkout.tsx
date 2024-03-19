86f1a0f18499a9a1cf98eee99ee7c59b9d6a847d
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2024-03-19 08:58:35 +0100

    Add new address form

--- components/checkout/Checkout.tsx
+++ components/checkout/Checkout.tsx
@@ -8,12 +8,16 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Checkout.module.scss';
 import DeliveryDetailsForm from './DeliveryDetails/DeliveryDetailsForm';
+import CheckBox from '@/shared/checkbox/Checkbox';
 
 const Checkout = () => {
 	const { theme, cartDetails } = useGlobalContext();
 	const [Open, setOpen] = useState<boolean>(false);
 	const [selectedOption, setSelectedOption] = useState<string>('');
 	const [selectedShippingOption, setSelectedShippingOption] = useState<string>('');
+	const [isGift, setIsGift] = useState<boolean>(false);
+	const [selectedAddressId, setSelectedAddressId] = useState<number>(0);
+	const [showContactForm, setShowContactForm] = useState<boolean>(false);
 	const open = () => {
 		setOpen(true);
 	};
@@ -52,6 +56,30 @@ const Checkout = () => {
 			id: '2',
 		},
 	];
+
+	const savedAddresses = [
+		{
+			id: 1,
+			address: 'No 1, Oladipo Diya Street, Gudu, Abuja',
+			phone_number: '+2348012345678',
+			email: 'test@gmail.com',
+			name: 'John Doe',
+		},
+		{
+			id: 2,
+			address: 'No 2, Oladipo Diya Street, Gudu, Abuja',
+			phone_number: '+2348012345678',
+			email: 'test2@gmail.com',
+			name: 'Jane Doe',
+		},
+		{
+			id: 3,
+			address: 'No 3, Oladipo Diya Street, Gudu, Abuja',
+			phone_number: '+2348012345678',
+			email: 'test3@gmail.com',
+			name: 'Sarah Doe',
+		},
+	];
 	return (
 		<div data-theme={theme} className={styles.checkout_container}>
 			<div className={styles.checkout_details}>
@@ -79,11 +107,13 @@ const Checkout = () => {
 						<div className={styles.delivery_header}>
 							<h3>Delivery details</h3>
 							<div className={styles.checkbox_container}>
-								<input className={styles.radio_field} type='checkbox' name='' id='' />
+								<CheckBox className={styles.radio_field} checked={isGift} onChange={(e) => setIsGift((prev) => !prev)} />
 								<p>This is a gift</p>
 							</div>
 						</div>
-						<DeliveryDetailsForm />
+						<div>
+							<DeliveryDetailsForm isGift={isGift} savedAddresses={savedAddresses} />
+						</div>
 					</div>
 					<div className={styles.shipping_details}>
 						<h3>Shipping method</h3>
@@ -91,7 +121,7 @@ const Checkout = () => {
 							{shippingOptions.map((option, index) => (
 								<div key={index} className={styles.select_date}>
 									<Radio onChange={() => setSelectedShippingOption(option.id)} checked={selectedShippingOption === option.id} />
-									<p>{option.name}</p>
+									<p data-active={selectedShippingOption === option.id}>{option.name}</p>
 								</div>
 							))}
 						</div>
@@ -102,10 +132,10 @@ const Checkout = () => {
 							{paymentOptions.map((option, index) => (
 								<div key={index} className={styles[option.type]}>
 									<Radio onChange={() => setSelectedOption(option.id)} className={styles.radio_field} checked={selectedOption === option.id} disabled={option.disabled} />
-									{option.id !== '3' && <h3>{option.name}</h3>}
+									{option.id !== '3' && <h3 data-active={selectedOption === option.id}>{option.name}</h3>}
 									{option.id === '3' && (
 										<div className={styles.icons}>
-											<h3>Pay with crypto</h3>
+											<h3 data-active={selectedOption === option.id}>Pay with crypto</h3>
 											<div className={styles.icon_container}>
 												{['/svgs/btc.svg', '/svgs/eth.svg', '/svgs/usdt.svg'].map((iconPath, index) => (
 													<div key={index} className={styles.icon}>
--- components/checkout/DeliveryDetails/DeliveryDetailsForm.tsx
+++ components/checkout/DeliveryDetails/DeliveryDetailsForm.tsx
@@ -1,27 +1,150 @@
-import { Button, InputField } from '@/shared';
-import React from 'react';
+import { Button, InputField, Radio } from '@/shared';
+import React, { useEffect, useState } from 'react';
 import styles from './DeliveryDetailsForm.module.scss';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { set } from 'nprogress';
 
-const DeliveryDetailsForm = () => {
+interface DeliveryDetailsFormProps {
+	isGift: boolean;
+	savedAddresses: any[];
+}
+const DeliveryDetailsForm = ({ isGift, savedAddresses }: DeliveryDetailsFormProps) => {
+	const schema = yup.object({
+		email: yup.string().email('Invalid email').required('Email is required'),
+		phone_number: yup.string().required('Phone number is required'),
+		name: yup.string().required('Name is required'),
+		address: yup.string().required('Address is required'),
+		delivery_note: yup.string(),
+		senders_email: isGift ? yup.string().email('Invalid email').required('Email is required') : yup.string(),
+		senders_phone_number: isGift ? yup.string().required('Phone number is required') : yup.string(),
+	});
+
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+		reset,
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
+	const [isBtnDisabled, setIsBtnDisabled] = useState<boolean>(true);
+	const watchAllFields = watch();
+	const [selectedAddress, setSelectedAddress] = useState<any>({});
+	const [showContactForm, setShowContactForm] = useState<boolean>(false);
+	const [newAddresses, setNewAddresses] = useState<any[]>([]);
+	console.log(watchAllFields);
+
+	useEffect(() => {
+		setNewAddresses(savedAddresses);
+	}, [savedAddresses]);
+
+	const handleContactAddressForm = (data: any) => {
+		console.log(data);
+		setNewAddresses([{ ...data, id: newAddresses.length }, ...newAddresses]);
+		setShowContactForm(false);
+		reset();
+	};
+
+	useEffect(() => {
+		if (watchAllFields.address && watchAllFields.name && watchAllFields.phone_number && watchAllFields.email) {
+			if (isGift) {
+				if (watchAllFields.senders_email && watchAllFields.senders_phone_number) {
+					setIsBtnDisabled(false);
+				} else {
+					setIsBtnDisabled(true);
+				}
+			} else {
+				setIsBtnDisabled(false);
+			}
+		} else {
+			setIsBtnDisabled(true);
+		}
+	}, [watchAllFields, isGift]);
+	console.log(newAddresses);
+
 	return (
-		<form>
-			<div className={styles.nameAndNumber}>
-				<InputField className={styles.input} placeholder='My email' />
-				<InputField
-					className={styles.input}
-					customPrefix={
-						<p className={styles.prefix_container}>
-							+234 <span className={styles.prefix_divider}></span>
-						</p>
-					}
-					type='number'
-					placeholder={`Receiver's phone number`}
-				/>
-			</div>
-			<InputField className={styles.input} placeholder='My full name' />
-			<InputField className={styles.input} placeholder='My address' />
+		<form onSubmit={handleSubmit(handleContactAddressForm)}>
+			{!showContactForm && savedAddresses.length > 0 ? (
+				<div className={styles.saved_addresses}>
+					{selectedAddress?.id ? (
+						<>
+							<div className={styles.single_address}>
+								<div className={styles.address_details}>
+									<h3>{selectedAddress.name}</h3>
+									<div className={styles.email_phone_container}>
+										<span>{selectedAddress.email}</span>
+										<span className={styles.dots} />
+										<span>{selectedAddress.phone_number}</span>
+									</div>
+									<p>{selectedAddress.address}</p>
+								</div>
+								<div className={styles.add_new_address}>
+									<p onClick={() => setSelectedAddress({})}>Change</p>
+								</div>
+							</div>
+						</>
+					) : (
+						<>
+							{newAddresses.map((address, index) => (
+								<div key={index} className={styles.address}>
+									<Radio checked={selectedAddress?.id === address.id} onChange={() => setSelectedAddress(address)} />
+									<div className={styles.address_details}>
+										<h3>{address.name}</h3>
+										<span className={styles.dots} />
+										<p>{address.address}</p>
+										<span className={styles.dots} />
+										<p>{address.phone_number}</p>
+									</div>
+								</div>
+							))}
+						</>
+					)}
+					<div data-visible={!selectedAddress?.id} className={styles.add_new_address}>
+						<p onClick={() => setShowContactForm(true)}>Add a new address</p>
+					</div>
+				</div>
+			) : (
+				<>
+					<div className={styles.nameAndNumber}>
+						<InputField className={styles.input} placeholder='My email' register={register('email')} />
+						<InputField
+							className={styles.input}
+							customPrefix={
+								<p className={styles.prefix_container}>
+									+234 <span className={styles.prefix_divider}></span>
+								</p>
+							}
+							type='number'
+							placeholder={`Receiver's phone number`}
+							register={register('phone_number')}
+						/>
+					</div>
+					<InputField className={styles.input} placeholder='My full name' register={register('name')} />
+					<InputField className={styles.input} placeholder='My address' register={register('address')} />
+				</>
+			)}
 			<InputField className={styles.input} inputClass={styles.delivery_note} placeholder='Delivery note (optional)' />
-			<Button disabled buttonType='transparent' className={styles.applyDiscount_btn}>
+			<div className={`${styles.sender_container} ${!isGift && styles.hide_sender}`}>
+				<h3>My details</h3>
+				<div className={` ${styles.nameAndNumber}`}>
+					<InputField className={styles.input} placeholder='My email' register={register('senders_email')} />
+					<InputField
+						className={styles.input}
+						customPrefix={
+							<p className={styles.prefix_container}>
+								+234 <span className={styles.prefix_divider}></span>
+							</p>
+						}
+						type='number'
+						placeholder={`My phone number`}
+						register={register('senders_phone_number')}
+					/>
+				</div>
+			</div>
+			<Button type='submit' disabled={isBtnDisabled} buttonType='transparent' className={`${styles.applyDiscount_btn} ${!isBtnDisabled && styles.applyDiscount_btnActive}`}>
 				<h4>Save Details</h4>
 			</Button>
 		</form>
