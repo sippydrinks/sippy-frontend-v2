22309f2dd0056433c1723a1dbbd61ccd9443717a
nano235 <revivaladolor@yahoo.com>
2024-04-10 07:38:00 +0100

    updates for netlify build

--- shared/Radio/Radio.tsx
+++ shared/Radio/Radio.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { useGlobalContext } from '@/contexts/AppContext';
-import { CheckboxProps } from '@/interface';
-import styles from './radio.module.scss';
+import React from "react";
+import { useGlobalContext } from "@/contexts/AppContext";
+import { CheckboxProps } from "@/interface";
+import styles from "./Radio.module.scssadio.module.scss";
 
-const Radio = ({ label, onChange, className, checked, value, disabled }: CheckboxProps) => {
+const Radio = ({
+	label,
+	onChange,
+	className,
+	checked,
+	value,
+	disabled,
+}: CheckboxProps) => {
 	const { theme } = useGlobalContext();
 	return (
 		<label className={styles.radio_container}>
 			{label}
-			<input type='radio' value={value} onChange={onChange} checked={checked} disabled={disabled} />
-			<span data-theme={theme} data-disabled={disabled ? 'yes' : 'no'} className={styles.circle}></span>
+			<input
+				type="radio"
+				value={value}
+				onChange={onChange}
+				checked={checked}
+				disabled={disabled}
+			/>
+			<span
+				data-theme={theme}
+				data-disabled={disabled ? "yes" : "no"}
+				className={styles.circle}
+			></span>
 		</label>
 	);
 };
